299719b9f0bf36afa45dc5a919f6c6c0627dbc6d
Oleksiy Chechel <alex.mirrr@gmail.com>
2018-05-26 20:40:54 +0300

    Add js scripts

---
+++ files/jss/main.js
@@ -0,0 +1,8 @@
+var $ = require("jquery");
+
+$(function () {
+    console.log($("body"));
+    console.log($("body").innerHeight());
+    console.log("Test test test");
+    $("#actions>.start").hide();
+})
